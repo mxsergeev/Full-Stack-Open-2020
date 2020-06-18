7d7f1abafcb0bb8b95526c21821ba264b80daf1c
MaxSer <themaximsergeev@gmail.com>
2020-06-18 16:20:49 +0300

    phonebook step6

--- part2/phonebook/src/app.js
+++ part2/phonebook/src/app.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/filter'
 import PersonForm from './components/personForm'
 import Persons from './components/persons'
+import axios from 'axios';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ searchOption, setSearchOption ] = useState('');
   const [ searchResult, setSearchResult ] = useState(persons);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(res => {
+      const data = res.data;
+      setPersons(data);
+    });
+  }, []);
 
   // clears name and number inputs
   const clear = () => {
