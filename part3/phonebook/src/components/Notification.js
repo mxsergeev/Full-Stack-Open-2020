6de8ab24b134cbfdc489fd8b9e15cc3ef3d308fc
MaxSer <themaximsergeev@gmail.com>
2020-10-01 10:34:04 +0300

    Set CSS property White-space to pre-wrap for showing error messages that are longer than one line.

--- part3/phonebook/src/components/Notification.js
+++ part3/phonebook/src/components/Notification.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Notification = ({ notification }) => {
     const notificationStyle = {
+        whiteSpace: 'pre-wrap',
         opacity: 0.8,
         width: '50%',
         padding: 5,
